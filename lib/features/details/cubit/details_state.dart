9d087c3e0b6b800edc7f9ddce3b8d3108c453313
Dominik Styrczula <z4j4c98@gmail.com>
2024-01-15 18:24:58 +0100

    Add Details Page

---
+++ lib/features/details/cubit/details_state.dart
@@ -0,0 +1,9 @@
+part of 'details_cubit.dart';
+
+ class DetailsState {
+  final ItemModel? itemModel;
+
+  DetailsState({required this.itemModel});
+ }
+
+
--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:cantwait28/features/add/page/add_page.dart';
+import 'package:cantwait28/features/details/pages/details_page.dart';
 import 'package:cantwait28/features/home/cubit/home_cubit.dart';
 import 'package:cantwait28/models/item_model.dart';
 import 'package:cantwait28/repositories/items_repository.dart';
@@ -17,7 +18,52 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Can\'t Wait 🤩'),
       ),
-      body: const _HomePageBody(),
+      body: BlocProvider(
+        create: (context) => HomeCubit(ItemsRepository())..start(),
+        child: BlocBuilder<HomeCubit, HomeState>(
+          builder: (context, state) {
+            final itemModels = state.items;
+
+            return ListView(
+              padding: const EdgeInsets.symmetric(
+                vertical: 20,
+              ),
+              children: [
+                for (final itemModel in itemModels)
+                  Dismissible(
+                    key: ValueKey(itemModel.id),
+                    background: const DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                      ),
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Padding(
+                          padding: EdgeInsets.only(right: 32.0),
+                          child: Icon(
+                            Icons.delete,
+                          ),
+                        ),
+                      ),
+                    ),
+                    confirmDismiss: (direction) async {
+                      // only from right to left
+                      return direction == DismissDirection.endToStart;
+                    },
+                    onDismissed: (direction) {
+                      context
+                          .read<HomeCubit>()
+                          .remove(documentID: itemModel.id);
+                    },
+                    child: _ListViewItem(
+                      itemModel: itemModel,
+                    ),
+                  ),
+              ],
+            );
+          },
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context).push(
@@ -33,60 +79,6 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class _HomePageBody extends StatelessWidget {
-  const _HomePageBody({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => HomeCubit(ItemsRepository())..start(),
-      child: BlocBuilder<HomeCubit, HomeState>(
-        builder: (context, state) {
-          final itemModels = state.items;
-     
-          return ListView(
-            padding: const EdgeInsets.symmetric(
-              vertical: 20,
-            ),
-            children: [
-              for (final itemModel in itemModels)
-                Dismissible(
-                  key: ValueKey(itemModel.id),
-                  background: const DecoratedBox(
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                    ),
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child: Padding(
-                        padding: EdgeInsets.only(right: 32.0),
-                        child: Icon(
-                          Icons.delete,
-                        ),
-                      ),
-                    ),
-                  ),
-                  confirmDismiss: (direction) async {
-                    // only from right to left
-                    return direction == DismissDirection.endToStart;
-                  },
-                  onDismissed: (direction) {
-                    context.read<HomeCubit>().remove(documentID: itemModel.id);
-                  },
-                  child: _ListViewItem(
-                    itemModel: itemModel,
-                  ),
-                ),
-            ],
-          );
-        },
-      ),
-    );
-  }
-}
-
 class _ListViewItem extends StatelessWidget {
   const _ListViewItem({
     Key? key,
@@ -97,75 +89,84 @@ class _ListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        vertical: 10,
-        horizontal: 30,
-      ),
-      child: Container(
-        decoration: const BoxDecoration(
-          color: Colors.black12,
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => DetailsPage(id: itemModel.id),
+          ),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 30,
         ),
-        child: Column(
-          children: [
-            Container(
-              height: 80,
-              decoration: BoxDecoration(
-                color: Colors.black12,
-                image: DecorationImage(
-                  image: NetworkImage(
-                    itemModel.imageURL,
+        child: Container(
+          decoration: const BoxDecoration(
+            color: Colors.black12,
+          ),
+          child: Column(
+            children: [
+              Container(
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.black12,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      itemModel.imageURL,
+                    ),
+                    fit: BoxFit.cover,
                   ),
-                  fit: BoxFit.cover,
                 ),
               ),
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(10),
+                      padding: const EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(
+                            itemModel.title,
+                            style: const TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            itemModel.releaseDate.toString.toString(),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.white70,
+                    ),
                     margin: const EdgeInsets.all(10),
                     padding: const EdgeInsets.all(10),
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
                         Text(
-                          itemModel.title,
+                          itemModel.daysLeft(),
                           style: const TextStyle(
                             fontSize: 20.0,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        const SizedBox(height: 10),
-                        Text(
-                          itemModel.releaseDate.toString.toString(),
-                        ),
+                        const Text('days left'),
                       ],
                     ),
                   ),
-                ),
-                Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.white70,
-                  ),
-                  margin: const EdgeInsets.all(10),
-                  padding: const EdgeInsets.all(10),
-                  child:  Column(
-                    children: [
-                      Text(
-                        itemModel.daysLeft(),
-                        style: const TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const Text('days left'),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
